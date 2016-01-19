d5af1eb6bd279100a269634b907d07dc613d157f
alex <alex@nabu.be>
2016-01-19 18:34:29 +0100

    Added service functionality to get the configuration

--- src/main/java/be/nabu/eai/module/configuration/ConfigurationArtifact.java
+++ src/main/java/be/nabu/eai/module/configuration/ConfigurationArtifact.java
@@ -2,32 +2,45 @@ package be.nabu.eai.module.configuration;
 
 import java.io.IOException;
 import java.nio.charset.Charset;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 import be.nabu.eai.repository.api.Repository;
-import be.nabu.libs.artifacts.api.Artifact;
 import be.nabu.libs.resources.api.ManageableContainer;
 import be.nabu.libs.resources.api.ReadableResource;
 import be.nabu.libs.resources.api.WritableResource;
 import be.nabu.libs.resources.api.Resource;
 import be.nabu.libs.resources.api.ResourceContainer;
+import be.nabu.libs.services.api.DefinedService;
+import be.nabu.libs.services.api.ExecutionContext;
+import be.nabu.libs.services.api.Service;
+import be.nabu.libs.services.api.ServiceException;
+import be.nabu.libs.services.api.ServiceInstance;
+import be.nabu.libs.services.api.ServiceInterface;
 import be.nabu.libs.types.api.ComplexContent;
 import be.nabu.libs.types.api.ComplexType;
+import be.nabu.libs.types.api.DefinedType;
+import be.nabu.libs.types.base.ComplexElementImpl;
 import be.nabu.libs.types.base.ValueImpl;
 import be.nabu.libs.types.binding.api.Window;
 import be.nabu.libs.types.binding.xml.XMLBinding;
 import be.nabu.libs.types.java.BeanResolver;
 import be.nabu.libs.types.properties.NillableProperty;
+import be.nabu.libs.types.structure.Structure;
 import be.nabu.utils.io.IOUtils;
 import be.nabu.utils.io.api.ByteBuffer;
 import be.nabu.utils.io.api.ReadableContainer;
 import be.nabu.utils.io.api.WritableContainer;
 
-public class ConfigurationArtifact implements Artifact {
+public class ConfigurationArtifact implements DefinedService {
 
 	private String id;
 	private Repository repository;
 	private ComplexContent configuration;
 	private ResourceContainer<?> directory;
+	private Structure input = new Structure();
+	private Structure output;
 	
 	public ConfigurationArtifact(String id, ResourceContainer<?> directory, Repository repository) {
 		this.id = id;
@@ -90,5 +103,51 @@ public class ConfigurationArtifact implements Artifact {
 		finally {
 			writable.close();
 		}
+	}
+
+	@Override
+	public ServiceInterface getServiceInterface() {
+		return new ServiceInterface() {
+			@Override
+			public ComplexType getInputDefinition() {
+				return input;
+			}
+			@Override
+			public ComplexType getOutputDefinition() {
+				if (output == null) {
+					synchronized(ConfigurationArtifact.this) {
+						Structure output = new Structure();
+						output.add(new ComplexElementImpl("configuration", getConfiguration().getType(), output));
+						ConfigurationArtifact.this.output = output;
+					}
+				}
+				return output;
+			}
+			@Override
+			public ServiceInterface getParent() {
+				return null;
+			}
+		};
+	}
+
+	@Override
+	public ServiceInstance newInstance() {
+		return new ServiceInstance() {
+			@Override
+			public Service getDefinition() {
+				return ConfigurationArtifact.this;
+			}
+			@Override
+			public ComplexContent execute(ExecutionContext executionContext, ComplexContent input) throws ServiceException {
+				ComplexContent newInstance = getServiceInterface().getOutputDefinition().newInstance();
+				newInstance.set("configuration", getConfiguration());
+				return newInstance;
+			}
+		};
+	}
+
+	@Override
+	public Set<String> getReferences() {
+		return new HashSet<String>(Arrays.asList(((DefinedType) getConfiguration().getType()).getId()));
 	}
 }
